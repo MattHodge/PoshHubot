c2eaa9828cbd9f56517835ff7df63a339c54e2fb
Matt Hodge <matthodge@gmail.com>
2014-11-14 20:10:05 -0600

    initial commit

---
+++ Functions/New-HuBotEnvironmentVariable.ps1
@@ -0,0 +1,51 @@
+﻿Set-StrictMode -Version Latest
+
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function New-HuBotEnvironmentVariable
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Name of the Environment Variable to set
+        [Parameter(Mandatory=$true)]
+        $Name,
+
+        # Value of the Environment Variable
+        [string]
+        $Value,
+
+        # Path To HuBotPowerShellConfig.json File
+        [string]
+        $configPath=[string](Split-Path -Parent $MyInvocation.MyCommand.Definition) + '\HuBotPowerShellConfig.json'
+    )
+
+    Begin
+    {
+        Write-Output $configPath
+
+        if(Test-Path -Path $configPath)
+        {
+            $Config = Get-Content -Path $configPath | Out-String | ConvertFrom-Json
+            Write-Output -InputObject $Config
+        }
+        else
+        {
+            throw "Unable to find HuBotPowerShellConfig.json at $($configPath)"
+        }
+    }
+    Process
+    {
+    }
+    End
+    {
+    }
+}
